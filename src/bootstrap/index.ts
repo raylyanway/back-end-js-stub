32acc2ad10512b68dd35d954450606286aa98ffb
iLia Railian <raylyanway@gmail.com>
2023-03-19 19:51:48 -0700

    delete extra

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -1,6 +1,5 @@
 import { Express } from "express";
 
-import { config } from "./config";
 import { db } from "./db";
 import { prod } from "./prod";
 import { routes } from "./routes";
@@ -10,6 +9,5 @@ export { logger } from "./logging";
 export const bootstrap = (app: Express) => {
   routes(app);
   db();
-  config();
   prod(app);
 };
