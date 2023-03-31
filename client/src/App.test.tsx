5dc4318b8a55a20f02761ffc3ab201d75cbb3aef
iLia Railian <raylyanway@gmail.com>
2023-03-30 18:12:46 -0700

    feat(*): update package json

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
 import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const server = setupServer(
+  rest.get("/part-of-speech", (req, res, ctx) => {
+    return res(ctx.json({ greeting: "hello there" }));
+  })
+);
+
+describe("App", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it("renders learn react link", () => {
+    render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    expect(/learn react/i).not.toBeInTheDocument();
+  });
 });
