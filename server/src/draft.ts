eb4d0579a35da7a65144c6e8866bf69cf0bc9943
iLia Railian <raylyanway@gmail.com>
2023-04-03 19:17:02 -0700

    feat(*): change draft

--- server/src/draft.ts
+++ server/src/draft.ts
@@ -1,4 +1,4 @@
-import { collect } from "./collector";
+import { collect } from "./openAI";
 import { partsOfSpeech } from "./parts_of_speech";
 import { translate } from "./translator";
 import { addPartsOfSpeechToDatabase, addWordsToDatabase } from "./utils";
@@ -31,73 +31,3 @@ async function App() {
 }
 
 export default App;
-
-function findWords(text: string) {
-  return text.match(/\w+/g) ?? [];
-}
-
-function findPunctuation(text: string) {
-  return text.match(/\W/g) ?? [];
-}
-
-function getAlignWords(words: string[]) {
-  return words.map((w) => w.toLowerCase());
-}
-
-function getCountItems(items: string[]) {
-  const uniqItems = Array.from(new Set(items));
-
-  return uniqItems.map((uniqItem) => ({
-    item: uniqItem,
-    count: items.filter((item) => item === uniqItem).length,
-  }));
-}
-
-function getPartsOfSpeech() {
-  return [
-    {
-      name: "noun",
-      description: "A noun is the name of a person, place, thing, or idea.",
-    },
-    {
-      name: "pronoun",
-      description: "A pronoun is a word used in place of a noun.",
-    },
-    { name: "verb", description: "A verb expresses action or being." },
-    {
-      name: "adjective",
-      description: "An adjective modifies or describes a noun or pronoun.",
-    },
-    {
-      name: "adverb",
-      description:
-        "An adverb modifies or describes a verb, an adjective, or another adverb.",
-    },
-    {
-      name: "preposition",
-      description:
-        "A preposition is a word placed before a noun or pronoun to form a phrase modifying another word in the sentence.",
-    },
-    {
-      name: "conjunction",
-      description: "A conjunction joins words, phrases, or clauses.",
-    },
-    {
-      name: "interjection",
-      description: "An interjection is a word used to express emotion.",
-    },
-  ];
-}
-
-function getStat(
-  text: string,
-  parser: (text: string) => string[],
-  modifier?: (items: string[]) => string[]
-) {
-  const items = parser(text);
-  const modifiedItems = modifier?.(items) ?? [];
-  const countItems = getCountItems(items);
-  const countModifiedItems = getCountItems(modifiedItems);
-
-  return { text, items, modifiedItems, countItems, countModifiedItems };
-}
